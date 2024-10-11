65ab30dba633419c37e250baa20da72c98425cd1
VJJJJR <victorjacome@unicomfacauca.edu.co>
2024-10-10 19:13:51 -0500

    Commit inicial del proyecto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+<hr/>
+<MyCounter :value="5"/>
+<MyCountercopy :value="5"/>
+<MyCountercopy3 :value="5"/>
+
+
+    </template>
+
+    <script lang="ts" setup>
+import MyCounter from './Components/MyCounter.vue';
+import MyCountercopy from './Components/MyCountercopy.vue';
+import MyCountercopy3 from './Components/MyCountercopy.vue';
+
+
+</script>
---
+++ src/Components/MyCountercopy3.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>hola mundo</h1>
+<h3>Contador: {{contador}}</h3>
+<h3>Cuadrado: {{ contador*contador}}</h3>
+
+<div>
+<button v-on:click="contador++">+1</button>
+<button v-on:click="contador--">-1</button>
+
+</div>
+
+    </template>
+
+   <script lang="ts" src="./MyCounter3"></script>
+    
+
+</script>
+<style scoped>
+h1{color: red;
+
+
+}
+
+</style>
